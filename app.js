bc9faa1da123b3836863f151fa0d9cdac4f78eda
Jack Park <jackpark@gmail.com>
2020-03-25 17:57:47 -0700

    raised image file size for drag&drop

--- app.js
+++ app.js
@@ -19,10 +19,12 @@ app.set('view engine', 'hbs');
 hbs.registerPartials(`${__dirname}/views/partials`);
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use(flash());
 app.use(session({
   genid(req) {
